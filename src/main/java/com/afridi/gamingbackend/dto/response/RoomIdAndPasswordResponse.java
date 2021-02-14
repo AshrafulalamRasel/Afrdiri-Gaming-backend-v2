76b6fb8d146967f20af9f6caf7e820175d33601d
MohaiminulHaq <mohaiminul35-117@diu.edu.bd>
2021-02-14 20:26:32 +0600

    roomidandpassword

---
+++ src/main/java/com/afridi/gamingbackend/dto/response/RoomIdAndPasswordResponse.java
@@ -0,0 +1,29 @@
+package com.afridi.gamingbackend.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfoResponse {
+
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+
+    private String email;
+
+    private String mobileNo;
+    private Double acBalance;
+    private int totalKill;
+    private Double totalEarn;
+
+
+}
